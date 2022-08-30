83d0437b8e169344fbc6a76e9b1979627d933163
Gavin Kyle Torres <111344196+GavinTorres09@users.noreply.github.com>
2022-08-30 22:35:53 +0800

    Create App.java

---
+++ exercise_1/App.java
@@ -0,0 +1,71 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Character Class
+        Character knight = new Character();
+
+        knight.strength = 10;
+        knight.agility = 5;
+        knight.intelligence = 0;
+        knight.name = "Chen";
+        knight.sayMyName();
+
+        Character mage = new Character();
+
+        mage.strength = 0;
+        mage.agility = 5;
+        mage.intelligence = 10;
+        mage.name = "Akasha";
+        mage.sayMyName();
+
+        Character thief = new Character();
+
+        thief.strength = 5;
+        thief.agility = 10;
+        thief.intelligence = 0;
+        thief.name = "Gondar";
+        thief.sayMyName();
+
+        // Enemy Class
+        Enemy boss = new Enemy();
+
+        boss.damage = 50;
+        boss.health = 500;
+        boss.armor = 100;
+        boss.hasMagicImmunity = true;
+        boss.name = "Roshan";
+        boss.enemyDialogue();
+
+        Enemy skeleton = new Enemy();
+
+        skeleton.damage = 5;
+        skeleton.health = 10;
+        skeleton.armor = 5;
+        skeleton.hasMagicImmunity = false;
+        skeleton.name = "Common Skeleton Warrior";
+        skeleton.enemyDialogue();
+
+        Enemy dragon = new Enemy();
+
+        dragon.damage = 40;
+        dragon.health = 400;
+        dragon.armor = 75;
+        dragon.hasMagicImmunity = false;
+        dragon.name = "Ancient Wyvern";
+        dragon.enemyDialogue();
+    
+//Exercise 1
+
+ // Creates a new object from Weapon class
+Weapon sword = new Weapon("Divine Rapier", 550, 10, "Legendary", "is a powerful weapon assembled with items from the Secret Shop.");
+sword.weaponDescription();
+
+Weapon Scepter = new Weapon("Dagon", 1000, 8, "Mythical", "A lesser wand that grows in power the longer it is used, it brings magic to the fingertips of the user.");
+Scepter.weaponDescription();
+
+Weapon axe = new Weapon("Battlefury", 120, 15, "Common", "The bearer of this mighty axe gains the ability to cut down swaths of enemies at once.");
+axe.weaponDescription();
+//Call the weaponDescription Method
+       
+        }
+    }
+
